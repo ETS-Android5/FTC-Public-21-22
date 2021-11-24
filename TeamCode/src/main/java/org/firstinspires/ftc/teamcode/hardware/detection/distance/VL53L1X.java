a6999ddeb654bc150a7511f873ba85b49eecac29
Owen Scott <owens3364@gmail.com>
2021-11-23 19:41:28 -0600

    API Translated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/detection/distance/VL53L1X.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/detection/distance/VL53L1X.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode.hardware.detection.distance;
 
+import android.util.Pair;
+
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
 import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
+import com.qualcomm.robotcore.util.TypeConversion;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
