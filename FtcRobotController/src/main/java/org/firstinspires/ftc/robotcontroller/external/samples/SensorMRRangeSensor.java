6b47b217081ec53e7a44a3f05bec6f41a3e4a93d
Kalya Kothavale <kalya.kothavale@gmail.com>
2021-12-02 19:53:04 -0600

    Sensor test

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRRangeSensor.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRRangeSensor.java
@@ -30,6 +30,7 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -48,24 +49,33 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * @see <a href="http://modernroboticsinc.com/range-sensor">MR Range Sensor</a>
  */
 @TeleOp(name = "Sensor: MR range sensor", group = "Sensor")
-@Disabled   // comment out or remove this line to enable this opmode
+
 public class SensorMRRangeSensor extends LinearOpMode {
 
-    ModernRoboticsI2cRangeSensor rangeSensor;
+    Rev2mDistanceSensor frontSensor;
+    Rev2mDistanceSensor backSensor;
+    Rev2mDistanceSensor leftSensor;
+    Rev2mDistanceSensor rightSensor;
 
     @Override public void runOpMode() {
 
         // get a reference to our compass
-        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
+        frontSensor = hardwareMap.get(Rev2mDistanceSensor.class, "FRONT_RANGE");
+        backSensor = hardwareMap.get(Rev2mDistanceSensor.class, "REAR_RANGE");
+        leftSensor = hardwareMap.get(Rev2mDistanceSensor.class, "LEFT_RANGE");
+        rightSensor = hardwareMap.get(Rev2mDistanceSensor.class, "RIGHT_RANGE");
 
         // wait for the start button to be pressed
         waitForStart();
 
         while (opModeIsActive()) {
-            telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
-            telemetry.addData("raw optical", rangeSensor.rawOptical());
-            telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
-            telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+
+            telemetry.addData("front cm", frontSensor.getDistance(DistanceUnit.CM));
+            telemetry.addData("back cm", backSensor.getDistance(DistanceUnit.CM));
+            telemetry.addData("left cm", leftSensor.getDistance(DistanceUnit.CM));
+            telemetry.addData("right cm", rightSensor.getDistance(DistanceUnit.CM));
+
+
             telemetry.update();
         }
     }
