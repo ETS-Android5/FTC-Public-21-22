5b3f917a25de8866095df10bd9a4a0a4c589b725
Owen Scott <owens3364@gmail.com>
2021-12-14 08:44:09 -0600

    Vision prolly works

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRRangeSensor.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorMRRangeSensor.java
@@ -49,7 +49,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * @see <a href="http://modernroboticsinc.com/range-sensor">MR Range Sensor</a>
  */
 @TeleOp(name = "Sensor: MR range sensor", group = "Sensor")
-
+@Disabled
 public class SensorMRRangeSensor extends LinearOpMode {
 
     Rev2mDistanceSensor frontSensor;
