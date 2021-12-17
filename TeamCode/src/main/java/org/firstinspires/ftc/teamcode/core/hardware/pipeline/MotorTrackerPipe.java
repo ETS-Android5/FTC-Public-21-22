6d7c6155467cc8123fc9d0ce19f6db102b8a984a
Kalya Kothavale <kalya.kothavale@gmail.com>
2021-12-16 21:01:15 -0600

    WIP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/hardware/pipeline/MotorTrackerPipe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/hardware/pipeline/MotorTrackerPipe.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.core.hardware.pipeline;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.core.annotations.hardware.RunMode;
+import org.firstinspires.ftc.teamcode.core.hardware.state.MotorPositionData;
 
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 public class MotorTrackerPipe extends HardwarePipeline {
   private static MotorTrackerPipe instance;
-  private final Map<String, Integer> motorPositions = new HashMap<>();
+  private final Map<String, MotorPositionData> motorPositions = new HashMap<>();
   private final List<CallbackData<Integer>> motorPositionCallbacks = new LinkedList<>();
   private boolean inPipe = false;
 
@@ -38,9 +39,17 @@ public class MotorTrackerPipe extends HardwarePipeline {
   }
 
   public int getPositionOf(String motorName) throws IllegalArgumentException {
-    Integer motorPosition = motorPositions.get(motorName);
-    if (motorPosition != null) {
-      return motorPosition;
+    MotorPositionData motorPositionData = motorPositions.get(motorName);
+    if (motorPositionData != null) {
+      return (int) motorPositionData.getCurrentTicks();
+    }
+    throw new IllegalArgumentException("That Motor is not tracked!");
+  }
+
+  public double getVelocity(String motorName) throws IllegalArgumentException {
+    MotorPositionData motorPositionData = motorPositions.get(motorName);
+    if (motorPositionData != null) {
+      return motorPositionData.velocity();
     }
     throw new IllegalArgumentException("That Motor is not tracked!");
   }
@@ -55,10 +64,15 @@ public class MotorTrackerPipe extends HardwarePipeline {
               if (m.getRunMode() == RunMode.RUN_TO_POSITION
                   || m.getRunMode() == RunMode.RUN_USING_ENCODER) {
                 int pos = ((DcMotor) hardware.get(m.getName())).getCurrentPosition();
-                motorPositions.put(m.getName(), pos);
+                MotorPositionData data = motorPositions.get(m.getName());
+                if (data != null) {
+                  data.addDataPoint(System.nanoTime(), pos);
+                } else {
+                  motorPositions.put(m.getName(), new MotorPositionData(System.nanoTime(), pos));
+                }
               }
             });
-    DataTracker.evaluateCallbacks(motorPositionCallbacks, motorPositions);
+    DataTracker.evaluateCallbacks(motorPositionCallbacks, motorPositions, (MotorPositionData data) -> (int) data.getCurrentTicks());
     inPipe = false;
     return super.process(hardware, r);
   }
