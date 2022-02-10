6a0867a52a68103ff0b4c4b87479f15b40d858c6
kkinneer <kkinneer@gmail.com>
2022-02-09 18:08:40 -0600

    Outreach bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/OutreachBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tele/OutreachBot.java
@@ -22,12 +22,12 @@ public class OutreachBot extends LinearOpMode {
 
         // RL is Right/Left servo.
         double tapeMeasureRLServoPosition = .5; // This is init position and tracking of current position
-        final double tapeMeasureRLServoTickMS = 200; // This is milliseconds between allowed movements of each tick
+        final double tapeMeasureRLServoTickMS = 100; // This is milliseconds between allowed movements of each tick
         final double tapeMeasureRLServoTick = .005; // This is amount of each tick
 
         // UD is Up/Down servo.
         double tapeMeasureUDServoPosition = .5; // This is init position and tracking of current position
-        final double tapeMeasureUDServoTickMS = 200; // This is milliseconds between allowed movements of each tick
+        final double tapeMeasureUDServoTickMS = 100; // This is milliseconds between allowed movements of each tick
         final double tapeMeasureUDServoTick = .005; // This is amount of each tick
 
         Servo rlServo = hardwareMap.servo.get("YAW_SERVO");
@@ -59,33 +59,33 @@ public class OutreachBot extends LinearOpMode {
             buttonTimerSinceLastButton = buttonTimerCurrentLoopTime - buttonTimerPrevLoopTime;
 
             if (gamepad1.a && buttonTimerSinceLastButton > tapeMeasureUDServoTickMS) {    // gamepad A is up
-                tapeMeasureUDServoPosition += tapeMeasureUDServoTick;
-                udServo.setPosition(tapeMeasureUDServoPosition);
-                buttonTimerPrevLoopTime = buttonTimerCurrentLoopTime;
-            }
-
-            if (gamepad1.y && buttonTimerSinceLastButton > tapeMeasureUDServoTickMS) {    // gamepad A is up
                 tapeMeasureUDServoPosition -= tapeMeasureUDServoTick;
                 udServo.setPosition(tapeMeasureUDServoPosition);
                 buttonTimerPrevLoopTime = buttonTimerCurrentLoopTime;
             }
 
-            if (gamepad1.dpad_right && buttonTimerSinceLastButton > tapeMeasureRLServoTickMS) {    // gamepad dpad_right is right
-                tapeMeasureRLServoPosition += tapeMeasureRLServoTick;
-                rlServo.setPosition(tapeMeasureRLServoPosition);
+            if (gamepad1.y && buttonTimerSinceLastButton > tapeMeasureUDServoTickMS) {    // gamepad A is up
+                tapeMeasureUDServoPosition += tapeMeasureUDServoTick;
+                udServo.setPosition(tapeMeasureUDServoPosition);
                 buttonTimerPrevLoopTime = buttonTimerCurrentLoopTime;
             }
 
-            if (gamepad1.dpad_left && buttonTimerSinceLastButton > tapeMeasureRLServoTickMS) {    // gamepad dpad_left is left
+            if (gamepad1.dpad_right && buttonTimerSinceLastButton > tapeMeasureRLServoTickMS) {    // gamepad dpad_right is right
                 tapeMeasureRLServoPosition -= tapeMeasureRLServoTick;
                 rlServo.setPosition(tapeMeasureRLServoPosition);
                 buttonTimerPrevLoopTime = buttonTimerCurrentLoopTime;
             }
 
+            if (gamepad1.dpad_left && buttonTimerSinceLastButton > tapeMeasureRLServoTickMS) {    // gamepad dpad_left is left
+                tapeMeasureRLServoPosition += tapeMeasureRLServoTick;
+                rlServo.setPosition(tapeMeasureRLServoPosition);
+                buttonTimerPrevLoopTime = buttonTimerCurrentLoopTime;
+            }
+
             if (gamepad1.dpad_up) {    // gamepad dpad_up extends the tape measure
-                extensionServo.setPosition(1);
-            } else if (gamepad1.dpad_down) {     // gamepad dpad_down retracts the tape measure
                 extensionServo.setPosition(0);
+            } else if (gamepad1.dpad_down) {     // gamepad dpad_down retracts the tape measure
+                extensionServo.setPosition(1);
             } else
                 extensionServo.setPosition(.5);  // stopped
 
